90528bfb5f7af3814ceca37786a008e00a4eca7c
Michael Collins <michaelcollins@msc.local>
2017-08-05 16:19:14 -0400

    initial simple service delivering a comma separate list, running on port 80, with no environment/configuration capabilities

---
+++ restaurantPickerServer.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  console.log('request received')
+  if (/GET.*basicRestaurantList/.exec(req.method + ' ' + req.url)) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'Content-Length': '58'
+      });
+    res.write('El Sombrero,La Piazza,Applebees,Ruby Tuesday,The Boathouse');
+  }
+  else {
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write('invalid resource');
+    //res.writeHead(200, { 'Content-Type': 'text/html'});
+    //res.write('did not match');
+  }
+  res.end();
+}).listen(80);
